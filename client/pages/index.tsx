59de350a3a4f318cd2a9367b5a2e2492717cdaac
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-10-23 00:04:06 +0530

    ➖ remove tailwind

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div>
-      <h1 className="text-orange-500">KzBlogs</h1>
+      <h1 className="">KzBlogs</h1>
     </div>
   );
 };
