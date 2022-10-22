0c3616afe96d6ed4e6e0b1aa0fcebf6b0154c30f
lakshya-dhariwal <lakshyadhariwal9@gmail.com>
2022-10-23 00:02:16 +0530

    🎉  Init Client

---
+++ client/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <h1 className="text-orange-500">KzBlogs</h1>
+    </div>
+  );
+};
+
+export default Home;
