dfbe25a07aa023a2ecdf11a35b07ed73c31eea66
Manaya20 <pachpor.manaya@gmail.com>
2023-06-19 00:03:49 +0530

    📝 jwt file added

--- jwt auth/jwt.py
+++ jwt auth/jwt.py
@@ -4,12 +4,11 @@ from typing import Dict
 import jwt
 from decouple import config
 
-
 JWT_SECRET = config("secret")
 JWT_ALGORITHM = config("algorithm")
 
 
-def token_response(token: str):
+def t_response(token: str):
     return {
         "access_token": token
     }
