4b189a7677a61c970f179c80846e4afcaa9d4702
Manaya20 <pachpor.manaya@gmail.com>
2023-06-26 01:28:12 +0530

    🎨 Add Login credentil routes

--- helpers/schema.py
+++ helpers/schema.py
@@ -1,24 +1,44 @@
 from pydantic import BaseModel
 from typing import Union
 
-
 class BlogSchema(BaseModel):
     blog_title: str
     blog_body: str
-    blog_entry_image: str
+    blog_entry_img: str
     author: str
+    date_published: int
+    date_modified: int
     readtime_min: int
-    date_modified: str
-    category: list[str]
+    likes_count: int
+    slug: str
+    category: list
     blog_publish_status: bool
 
 
 class UpdateBlogSchema(BaseModel):
-    blog_title: Union[str, None] = None
-    blog_body: Union[str, None] = None
-    blog_entry_image: Union[str, None] = None
-    author: Union[str, None] = None
-    readtime_min: Union[int, None] = None
-    date_modified: Union[str, None] = None
-    category: list[Union[str, None]] = None
-    blog_publish_status: Union[bool, None] = None
+    blog_title: str
+    blog_body: str
+    blog_entry_img: str
+    date_published: int
+    date_modified: int
+    readtime_min: int
+    id: str
+    likes_count: int
+    category: list
+    blog_publish_status: bool
+
+
+class AddBlogSchema(BaseModel):
+    blog_title: str
+    blog_body: str
+    blog_entry_img: str
+    author: str
+    category: list
+    blog_publish_status: bool
+
+class AddUserSchema(BaseModel):
+    name: str
+    email:str
+    password:str
+    
+
---
+++ routes/adminRoutes.py
@@ -0,0 +1,136 @@
+from fastapi import Response, Request, APIRouter as Router
+from json import dumps
+from db import database
+
+from helpers.schema import AddBlogSchema, UpdateBlogSchema,AddUserSchema
+
+router = Router()
+db = database.MongoDbConnection()
+
+@router.post("/add/user")
+async def add_user(req: Request, data: AddUserSchema)-> Response:
+    try:
+        output: str=db.add_user(data)
+        return Response(
+            dumps(
+            {"status": True,"message":"User added successfully!","user":output}
+            ),
+            status_code=200,
+            media_type="application/json",
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            dumps({"status": False, "message": "Oops! something went wrong"}),
+            status_code=500,
+            media_type="application/json",
+        )
+    
+@router.post("/login")
+async def login(emai:str, password: str, req: Request, data: AddUserSchema)-> Response:
+    try:
+        email=db.login(str(email))
+        password=db.login(str(password))
+        return Response
+    except Exception as e:
+        print(e)
+        return Response(
+            dumps({"status": False, "message": "Oops! something went wrong"}),
+            status_code=500,
+            media_type="application/json",
+        )
+ 
+
+
+@router.post("/add/blog")
+async def add_blog(req: Request, data: AddBlogSchema) -> Response:
+    try:
+        output: str = db.add_blog(data)
+        return Response(
+            dumps(
+                {"status": True, "message": "Blog added successfully!", "id": output}
+            ),
+            status_code=200,
+            media_type="application/json",
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            dumps({"status": False, "message": "Oops! something went wrong"}),
+            status_code=500,
+            media_type="application/json",
+        )
+
+
+@router.delete("/delete/blog/{id}")
+async def delete_blog(req: Request, id: str) -> Response:
+    try:
+        output = db.delete_blog(id)
+        return Response(
+            dumps(output),
+            status_code=200 if output["status"] else 404,
+            media_type="application/json",
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            dumps({"status": False, "message": "Oops! something went wrong"}),
+            status_code=500,
+            media_type="application/json",
+        )
+
+
+@router.put("/update/blog")
+async def update_blog(req: Request, data: UpdateBlogSchema) -> Response:
+    try:
+        output = db.update_blog(str(data.id), data.dict())
+        return Response(
+            dumps(output),
+            status_code=200 if output["status"] else 404,
+            media_type="application/json",
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            dumps({"status": False, "message": "Oops! something went wrong"}),
+            status_code=500,
+            media_type="application/json",
+        )
+
+
+@router.get("/blog/all")
+async def get_all(req: Request) -> Response:
+    try:
+        results: list = db.get_blogs(None, show_all=True)
+        return Response(dumps(results), status_code=200, media_type="application/json")
+    except Exception as e:
+        print(e)
+        return Response(
+            dumps({"status": False, "message": "Oops! something went wrong"}),
+            status_code=500,
+            media_type="application/json",
+        )
+
+
+@router.post("/update-status/{id}")
+async def update_status(req: Request, id: str) -> Response:
+    try:
+        blog = db.get_blog_by_id(str(id))
+        blog["blog_publish_status"] = not blog["blog_publish_status"]
+        output: dict = db.update_blog(blog["id"], blog)
+        output["publish_status"] = blog["blog_publish_status"]
+        return Response(
+            dumps(output),
+            status_code=200 if output["status"] else 404,
+            media_type="application/json",
+        )
+    except Exception as e:
+        print(e)
+        return Response(
+            dumps({"status": False, "message": "Oops! something went wrong"}),
+            status_code=500,
+            media_type="application/json",
+        )
+
+
+
