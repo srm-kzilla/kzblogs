0584c712293e1734ee0b765c2fa3023658c4984b
Mohamed Sami <srmf2025@gmail.com>
2023-06-28 16:27:00 +0530

    chore: remove unused fonts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -23,9 +23,7 @@ module.exports = {
         'card-shadow': 'rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;'
       },
       fontFamily:{
-        'font-body':['cal-sans'],
-        josefin_sans: ['Josefin Sans', 'sans-serif'],
-        outfit: ['Outfit', 'sans-serif'],
+        body:['cal-sans', 'sans-serif'],
       }
     },
   },
