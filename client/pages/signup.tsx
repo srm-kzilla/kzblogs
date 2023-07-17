fdcf4a3782143a0631686936308b92412e2f3b7f
ShivamBansal07 <sb2368@srmist.edu.in>
2023-07-18 02:10:21 +0530

    💄 Updated UI for Dark Mode

--- client/pages/signup.tsx
+++ client/pages/signup.tsx
@@ -8,20 +8,19 @@ const LoginPage: NextPage = () => {
         <title>Signup</title>
         <link rel="icon" href="/logo.svg" />
       </Head>
-      <div className="flex h-screen w-full flex-col bg-kz-dark-grey">
+      <div className="flex h-screen w-full flex-col bg-[#151519]">
         <div className="z-20">
-          <Navbar/>
+          <Navbar />
         </div>
         <div className="flex h-full w-full">
+          <div className="absolute bottom-0">
+            <img src="bgImage.svg" alt="" className=" w-vw" />
+          </div>
           <div className="hidden h-full w-5/12 lg:block">
-            <div className="flex w-vw justify-center h-[50vh]">
-              <div className="absolute w-5/12 h-1/2">
-                <img src="smallCircles.svg" alt="smallCircles" className="absolute w-[4.6vw] z-0 right-56 top-16" />
-                <img src="bigCircles.svg" alt="bigCircles" className="absolute w-[13.8vw] z-0 right-1 top-16" />
-              </div>
-              <div className="h-[50vw]">
+            <div className="flex justify-center h-[50vh]">
+              <div className="justify-center h-[50vh]">
                 <div className="relative h-1/2 pr-12">
-                  <div className="mt-20 h-full w-full font-josefinSans font-bold text-transparent text-[7.5vw] bg-clip-text bg-gradient-to-r from-kz-blue from-5% via-kz-green via-20% to-kz-orange to-40%">
+                  <div className="mt-20 h-full w-full font-josefinSans font-bold text-transparent text-[7.5vw] bg-clip-text bg-gradient-to-r from-kz-deepblue from-20% via-kz-lightblue via-45% to-kz-orange to-70%">
                     KZBlogs
                   </div>
                   {/* edit this tagline to an actual tagline */}
@@ -30,36 +29,36 @@ const LoginPage: NextPage = () => {
               </div>
             </div>
           </div>{/*Sign*/}
-          <div className="z-0 flex h-full w-full lg:w-7/12 items-center justify-center bg-zinc-900">
-            <div className="flex relative h-fit w-fit lg:w-7/12 xl:w-5/12 flex-col rounded-xl bg-kz-dark-grey px-10 py-12 sm:py-16 space-y-8 drop-shadow-2xl outline outline-1 outline-kz-orange shadow-box-shadow">
-              <div className="hidden absolute mb-24 -rotate-90 -left-24 md:block">
-                <h1 className="text-4xl font-bold text-zinc-800">Sign Up</h1>
+          <div className="z-0 flex h-full w-full lg:w-7/12 items-center justify-center">
+            <div className="flex relative h-fit w-fit lg:w-7/12 xl:w-5/12 flex-col rounded-xl bg-kz-dark-grey px-10 py-12 sm:py-16 sm:pt-10 space-y-8 drop-shadow-2xl outline outline-1 shadow-3xl">
+              <div className="absolute left-4 -top-[8vh] lg:-rotate-90 lg:-left-[15vh] lg:top-[7vh]">
+                <h1 className="text-[6vh] font-outfit font-bold text-kz-textgrey">Sign Up</h1>
               </div>
               <div>
-                <p className=" text-red-500 text-left font-outfit font-medium text-xl md:text-2xl m-1">Username</p>
+                <p className=" text-kz-textblue text-left font-outfit font-semibold text-xl md:text-2xl m-1">Username</p>
                 <div className="flex flex-col items-center">
-                  <div className="bg-zinc-700 w-full p-2 flex items-center rounded-xl ">
-                    <input className="bg-zinc-700 text-white outline-none w-full pl-2 text-sm " type="email" name="email" placeholder="Enter your user name" />
+                  <div className="w-full p-2 flex items-center rounded-xl ">
+                    <input className="bg-transparent border-b-2 text-kz-textgrey font-medium outline-none w-full text-sm" type="email" name="email" placeholder="Enter your user name" />
                   </div>
                 </div>
               </div>
               <div>
-                <p className=" text-red-500 text-left font-outfit font-medium text-xl md:text-2xl m-1">Password</p>
+                <p className=" text-kz-textblue text-left font-outfit font-semibold text-xl md:text-2xl m-1">Password</p>
                 <div className="flex flex-col items-center">
-                  <div className="bg-zinc-700 w-full p-2 flex items-center rounded-xl">
-                    <input className="bg-zinc-700 text-white outline-none w-full pl-2 text-sm " type="email" name="email" placeholder="Enter your Password" />
+                  <div className=" w-full p-2 flex items-center rounded-xl">
+                    <input className="bg-transparent border-b-2 text-kz-textgrey font-medium outline-none w-full text-sm" type="email" name="email" placeholder="Enter your Password" />
                   </div>
                 </div>
               </div>
               <div>
                 <div className="flex items-center justify-center">
-                  <hr className="my-12 h-0.5 w-full border-t-0 bg-neutral-400 opacity-50" />
+                  <hr className="my-8 h-0.5 w-full border-t-0 bg-neutral-400 opacity-50" />
                   <div className="m-2 text-neutral-400 opacity-60">or</div>
-                  <hr className="my-12 h-0.5 w-full border-t-0 bg-neutral-400 opacity-50" />
+                  <hr className="my-8 h-0.5 w-full border-t-0 bg-neutral-400 opacity-50" />
                 </div>
                 <div className="">
                   <div className="px-2 sm:px-5 max-w-sm ">
-                    <button type="button" className="text-white text-xs w-full bg-kz-orange  hover:shadow-box-shadow focus:outline-none font-medium rounded-lg md:text-sm px-5 py-2.5 text-center inline-flex items-center justify-between mr-2 mb-2">
+                    <button type="button" className="text-white text-xs w-full bg-kz-deepblue hover:shadow-box-shadow focus:outline-none font-medium rounded-lg md:text-sm px-5 py-2.5 text-center inline-flex items-center justify-between mr-2 mb-2">
                       <img src="google.svg" alt="Google-icon" className="h-5 sm:h-6" />
                       Sign up with Google
                       <div></div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,13 +8,16 @@ module.exports = {
     extend: {
       colors:{
         'kz-dark-grey':'#151319',
-        'kz-orange': '#FF5F5F',
+        'kz-orange':'#e94560',
         'kz-grey' : '#F9F9F9',
-        'kz-green': '#56A86E',
-        'kz-blue' : '#23BCFE',
+        'kz-textgrey' : '#8E8787',
+        'kz-textblue' : '#4e4feb',
+        'kz-lightblue': '#569ed6',
+        'kz-deepblue' : '#3b3ba6',
       },
       boxShadow:{
         'box-shadow': '0px 3px 16px rgba(255, 95, 95, 0.24)',
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
         'card-shadow': 'rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(0, 0, 0, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;'
       },
       fontFamily:{
