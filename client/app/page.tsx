188b7e4457a63febdf75c885a9de9891b9c35bbf
SaanviKumar13 <saanviprashantkumar@gmail.com>
2023-09-18 22:37:34 +0530

    fix: remove blogcard from page

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,13 +1,10 @@
 import Image from "next/image";
-import BlogCard from "./components/BlogCard";
 
 export default function Home() {
   return (
     <div>
       <h1 className="">KZBlogs</h1>
-      <div>
-        <BlogCard />
-      </div>
+      <div></div>
     </div>
   );
 }
