2417c98accbfad683e2f11a3d037e560d4cb0a17
SaanviKumar13 <saanviprashantkumar@gmail.com>
2023-09-19 21:14:56 +0530

    feat: makes navbar responsive

--- client/app/components/Navbar.tsx
+++ client/app/components/Navbar.tsx
@@ -7,24 +7,24 @@ import { PenSquare } from "lucide-react";
 const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
-    <div className="shadow-box p-2 font-body flex flex-row items-center">
+    <div className="shadow-box p-2 font-body flex lg:flex-row flex-col lg:items-center">
       <div className="flex flex-row items-center gap-5">
         <Image src={"logo.svg"} alt={"logo"} width={32} height={32} />
         <h1 className="text-4xl mt-2 font-extrabold bg-gradient-to-r from-kz-highlightd via-kz-highlightl to-kz-secondary bg-clip-text text-transparent">
           KZBlogs
         </h1>
-        <button
-          className="absolute w-10 h-10 right-1 top-6 cursor-pointer lg:hidden"
-          type="button"
-          onClick={() => setNavbarOpen(!navbarOpen)}
-        >
-          <Image src="menu.svg" alt="Menu" width={32} height={32} />
-        </button>
       </div>
+      <button
+        className="absolute w-10 h-10 right-1 top-6 cursor-pointer lg:hidden"
+        type="button"
+        onClick={() => setNavbarOpen(!navbarOpen)}
+      >
+        <Image src="menu.svg" alt="Menu" width={32} height={32} />
+      </button>
       <div
         className={
-          "lg:flex flex-grow justify-center text-kz-secondary lg:gap-6" +
-          (navbarOpen ? " flex flex-col" : " hidden")
+          "lg:flex flex-grow justify-center items-center text-kz-secondary lg:gap-6" +
+          (navbarOpen ? " flex flex-col items-end" : " hidden")
         }
       >
         <Link
@@ -48,20 +48,20 @@ const Navbar = () => {
       </div>
       <div
         className={
-          "lg:flex text-kz-secondary lg:gap-6 mr-4" +
+          "lg:flex items-center text-kz-secondary lg:gap-6 lg:mr-4" +
           (navbarOpen ? " flex flex-col" : " hidden")
         }
       >
         <Link
           href="/write"
-          className=" flex flex-row gap-3 items-center px-3 py-1 rounded-lg bg-[#3AA39B] text-kz-secondary hover:text-[#3AA39B] hover:bg-kz-secondary"
+          className="flex flex-row gap-3 items-center lg:px-3 lg:py-1 rounded-lg lg:bg-[#3AA39B] text-kz-secondary hover:text-[#3AA39B] lg:hover:bg-kz-secondary"
         >
-          <PenSquare />
+          <PenSquare className="hidden lg:visible" />
           Write
         </Link>
         <Link
           href="/signin"
-          className="px-3 py-1 rounded-lg bg-[#3AA39B] text-kz-secondary active:text-[#3AA39B] hover:text-[#3AA39B] hover:bg-kz-secondary"
+          className="lg:px-3 lg:py-1 rounded-lg lg:bg-[#3AA39B] text-kz-secondary hover:text-[#3AA39B] lg:hover:bg-kz-secondary"
         >
           Sign In
         </Link>
