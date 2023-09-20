5bdca788c05c7996b8c8e55e04d54cc1d322b3a4
ShivamBansal07 <sb2368@srmist.edu.in>
2023-09-21 00:22:33 +0530

    update ui and made recommended changes

--- client/app/components/BookmarkCard.tsx
+++ client/app/components/BookmarkCard.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { UserCircle2, PlusCircleIcon } from "lucide-react";
+import { UserCircle2 } from "lucide-react";
 import { Blog } from "../types";
 
 interface BookmarkCardProps {
@@ -13,7 +13,7 @@ const BookmarkCard = ({ blogs }: BookmarkCardProps) => {
   return (
     <div>
       {bookmarkNumber === 0 ? (
-        <div className="bg-gradient-to-tr from-[#0A427D] via-[#3AA39B] via-70% to-[#D9D9D9] to-100% p-4 w-full h-fit text-kz-secondary rounded-xl">
+        <div className="bg-gradient-to-tr from-kz-highlightd via-kz-highlightl via-70% to-kz-secondary to-100% p-4 w-full h-fit text-kz-secondary rounded-xl">
           <div className="flex-col">
             <h1 className="text-lg md:text-2xl ">Bookmarks</h1>
             <p className="mt-3 font-sans text-xs">
@@ -24,19 +24,16 @@ const BookmarkCard = ({ blogs }: BookmarkCardProps) => {
         </div>
       ) : (
         <div className="bg-kz-darkcard p-4 w-full h-fit text-kz-secondary rounded-xl">
-            <h1 className="text-2xl text-transparent bg-clip-text bg-gradient-to-tr from-[#0A427D] via-[#3AA39B] via-40% to-[#D9D9D9] to-90%">Bookmarked ( {bookmarkNumber} )</h1>
+            <h1 className="text-xl text-transparent bg-clip-text bg-gradient-to-tr from-kz-highlightd via-kz-highlightl via-40% to-kz-secondary to-90%"> Bookmarked ( {bookmarkNumber} )</h1>
           <div className="flex flex-col mt-3">
             {blogs.map(({ bookmarked, id, name, author }) =>
               bookmarked === true ? (
                 <div key={id} className="flex items-center justify-left my-3">
                   <button className="mx-2">
-                    <p className="text-xs max-w-[30px] whitespace-nowrap overflow-hidden font-extralight font-sans">
-                      {author}
-                    </p>
                     <UserCircle2 width={30} height={30} />
                   </button>
-                  <div className="mx-2">
-                    <h1>{name}</h1>
+                  <div className="mx-1 text-ellipsis">
+                    <p className="text-sm">{name}</p>
                     <p className="text-xs font-extralight font-sans">
                       subtitle
                       {/* subtitle data source not set yet */}
@@ -46,8 +43,8 @@ const BookmarkCard = ({ blogs }: BookmarkCardProps) => {
               ) : null
             )}
           </div>
-          <div className="text-kz-primary font-sans text-sm flex justify-center items-center">
-            <button className="bg-kz-secondary px-4 py-1 rounded-3xl">
+          <div className="text-kz-primary font-sans text-xs flex justify-center items-center">
+            <button className="bg-kz-secondary px-3 py-1 rounded-3xl">
               See All
             </button>
           </div>
