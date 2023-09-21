4c7bd41d561f7834ed4c36a35616b312f13f8e0c
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2023-09-21 14:00:44 +0530

    chore: Rewrite backend (#45)
    * chore: remove old backend code
    
    * fix: remove author and license from pyproject
    
    * fix: put required values in pyproject.toml
    
    * fix: python version to 3.9

--- helpers/constants.py
+++
@@ -1,8 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv(".env")
-
-CONST_DB_SETTINGS = {"MONGO_DB_URI": os.environ.get("MONGO_DB_URI")}
-CONST_ENVIRONMENT = os.environ.get("ENVIRONMENT")
