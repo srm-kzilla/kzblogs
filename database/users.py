347370d2ddd7fcfebc273c59fc0e24533ac939c4
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2023-09-22 14:21:04 +0530

    feat: add get_bookmarks function in database

--- database/users.py
+++ database/users.py
@@ -46,3 +46,13 @@ class Users:
         if output.modified_count == 0:
             return {"status": False, "message": "User does not exist"}
         return {"status": True, "message": "Bookmark removed successfully"}
+
+    def get_bookmarks(self, user_id: str):
+        user = self.users.find_one({"_id": ObjectId(user_id)})
+        if not user:
+            return {"status": False, "message": "User does not exist"}
+        bookmarks = user.get("bookmarks", [])
+        bookmarks = list(
+            self.blogs.find({"_id": {"$in": bookmarks}}) if bookmarks else []
+        )
+        return bookmarks
