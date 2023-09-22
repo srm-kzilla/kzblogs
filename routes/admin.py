a7d02cd97824172facb70b4f7cd4f334b3bd7aab
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2023-09-22 11:50:16 +0530

    feat: add admin create and update blog routes

--- routes/admin.py
+++ routes/admin.py
@@ -1,16 +1,32 @@
 from fastapi import Request, Response, APIRouter as Router
 from json import dumps
 from database import MongoDBConnection as Database
+from helpers.schemas import Blog
 
 
 router = Router()
 db = Database()
 
+
 @router.get("/{id}")
 async def get_blog(request: Request, id: str = "all", show_all: bool = True):
-    if id == "all": id = None
+    if id == "all":
+        id = None
     result = db.blogs.get_blog(id, show_all)
     return Response(content=dumps(result), media_type="application/json")
+
+
+@router.post("/")
+async def create_blog(request: Request, blog: Blog):
+    result = db.blogs.create_blog(dict(blog))
+    return Response(content=dumps(result), media_type="application/json")
+
+
+@router.put("/{id}")
+async def update_blog(request: Request, id: str, blog: Blog):
+    result = db.blogs.update_blog(id, dict(blog))
+    return Response(content=dumps(result), media_type="application/json")
+
 
 @router.delete("/{id}")
 async def delete_blog(request: Request, id: str):
