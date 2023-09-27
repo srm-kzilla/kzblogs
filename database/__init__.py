55c457eb8f30b12ee7037618ae51fb63be5b5a0c
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2023-09-27 18:47:19 +0530

    feat: asynchronous database functions

--- database/__init__.py
+++ database/__init__.py
@@ -1,12 +1,13 @@
-from pymongo import MongoClient
-from pymongo.database import Database
-from database.blogs import Blogs
-from database.users import Users
+from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.constants import DB_SETTINGS
+from database.blogs import Blog
+from database.users import User
 
-client = MongoClient(DB_SETTINGS.MONGODB_URI)
-class MongoDBConnection:
+client = AsyncIOMotorClient(DB_SETTINGS.MONGODB_URI)
+
+
+class Database:
     def __init__(self):
-        self.db = Database(client=client, name=DB_SETTINGS.DB_NAME)
-        self.blogs = Blogs(client, self.db)
-        self.users = Users(client, self.db)
+        self.db = client[DB_SETTINGS.DB_NAME]
+        self.blogs = Blog(client, self.db)
+        self.users = User(client, self.db)
--- database/blogs.py
+++ database/blogs.py
@@ -1,75 +1,71 @@
-from pymongo import MongoClient
-from pymongo.database import Database
-from bson import ObjectId
-from typing import Union
-
 from helpers.constants import DB_SETTINGS
-from helpers.schemas import Comment
+from typing import Union
+from bson import ObjectId
+from motor.motor_asyncio import AsyncIOMotorClient
 
 
-class Blogs:
-    def __init__(self, client: MongoClient, database: Database):
+class Blog:
+    def __init__(self, client: AsyncIOMotorClient, db):
         self.client = client
-        self.db = database
+        self.db = client[DB_SETTINGS.DB_NAME]
         self.blogs = self.db[DB_SETTINGS.BLOGS]
-        self.comments = self.db[DB_SETTINGS.COMMENTS]
         self.users = self.db[DB_SETTINGS.USERS]
+        self.comments = self.db[DB_SETTINGS.COMMENTS]
 
-    def get_blog(
-        self, blog_id: Union[str, None] = None, show_all=False
-    ) -> Union[dict, list]:
+    async def get_blog(self, blog_id: Union[str, None] = None, show_all: bool = True):
         if not blog_id:
-            for blog in self.blogs.find(
+            async for blog in self.blogs.find(
                 {"publish_status": True} if not show_all else {}
-            ):
+            ).to_list():
                 blog["_id"] = str(blog["_id"])
+                return blog
         blog = self.blogs.find_one({"_id": ObjectId(blog_id)})
         if blog:
             blog["_id"] = str(blog["_id"])
             return blog
         return {"status": False, "message": "Blog does not exist"}
 
-    def create_blog(self, blog: dict):
-        output = self.blogs.insert_one(blog)
+    async def create_blog(self, blog: dict):
+        output = await self.blogs.insert_one(blog)
         return {
             "status": True,
             "message": "Blog created successfully",
             "id": str(output.inserted_id),
         }
 
-    def delete_blog(self, id: str):
-        output = self.blogs.delete_one({"_id": ObjectId(id)})
+    async def delete_blog(self, id: str):
+        output = await self.blogs.delete_one({"_id": ObjectId(id)})
         if output.deleted_count == 0:
             return {"status": False, "message": "Blog does not exist"}
         return {"status": True, "message": "Blog deleted successfully"}
 
-    def update_blog(self, id: str, blog: dict):
-        output = self.blogs.update_one({"_id": ObjectId(id)}, {"$set": blog})
+    async def update_blog(self, id: str, blog: dict):
+        output = await self.blogs.update_one({"_id": ObjectId(id)}, {"$set": blog})
         if output.matched_count == 0:
             return {"status": False, "message": "Blog does not exist"}
         return {"status": True, "message": "Blog updated successfully"}
 
-    def add_like(self, id: str, user_id: str):
-        output = self.blogs.update_one(
+    async def add_like(self, id: str, user_id: str):
+        output = await self.blogs.update_one(
             {"_id": ObjectId(id)}, {"$push": {"likes": user_id}}
         )
         if output.modified_count == 0:
             return {"status": False, "message": "Blog does not exist"}
         return {"status": True, "message": "Like added successfully"}
 
-    def remove_like(self, id: str, user_id: str):
-        output = self.blogs.update_one(
+    async def remove_like(self, id: str, user_id: str):
+        output = await self.blogs.update_one(
             {"_id": ObjectId(id)}, {"$pull": {"likes": user_id}}
         )
         if output.modified_count == 0:
             return {"status": False, "message": "Blog does not exist"}
         return {"status": True, "message": "Like removed successfully"}
 
-    def add_comment(self, comment: Comment):
-        if self.blogs.count_documents({"_id": ObjectId(comment.blog_id)}) == 0:
+    async def add_comment(self, id: str, comment: dict):
+        if self.blogs.count_documents({"_id": ObjectId(id)}) == 0:
             return {"status": False, "message": "Blog does not exist"}
-        self.comments.insert_one(dict(comment))
+        await self.comments.insert_one(dict(comment))
         return {"status": True, "message": "Comment added successfully"}
 
-    def get_comments(self, blog_id):
-        return list(self.comments.find({"blog_id": blog_id}))
+    async def get_comments(self, blog_id: str):
+        return list(self.comments.find({"blog_id": ObjectId(blog_id)}))
