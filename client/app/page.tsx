db34b0677dbf1e99f8a253bea4fb6d4c9acf730f
ShivamBansal07 <sb2368@srmist.edu.in>
2023-09-27 14:35:50 +0530

    fix: Made Home more responsive

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -11,23 +11,23 @@ export default function Home() {
   return (
     <div>
       <Navbar />
-      <div className="flex w-full mt-24 justify-between gap-12">
-        <div className="w-9/12 flex justify-end">
-          <div className="">
+      <div className="flex w-full md:px-10 lg:px-20 mt-12 md:mt-24 justify-between">
+        <div className="w-full md:w-9/12 flex justify-center md:justify-start">
+          <div>
             <div className="font-serif text-3xl text-kz-secondary m-3">What’s <span className="text-kz-highlight-light">New</span> </div>
             {publishedBlogs.map((blogs: Blog) => (
-              <div key={blogs.id} className="w-[50vw] m-3">
+              <div key={blogs.id} className="w-[80vw] mb-10 md:w-[50vw] m-3">
                 <BlogCard {...blogs} />
               </div>
             ))}
           </div>
         </div>
-        <div className="flex w-3/12 justify-start mr-20 mt-10">
-          <div className="">
-            <div className="w-[20vw] m-6">
+        <div className="hidden md:flex w-3/12 justify-end mt-10">
+          <div>
+            <div className="md:w-[25vw] lg:w-[17vw] m-6">
               <DraftCard blogs={blogData} />
             </div>
-            <div className="w-[20vw] m-6">
+            <div className="md:w-[25vw] lg:w-[17vw] m-6">
               <TrendingCard blogs={blogData} />
             </div>
           </div>
