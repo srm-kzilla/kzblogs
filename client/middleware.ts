08666e283ec0e21588388c9b7eb4594f86a04dbf
SaanviKumar13 <saanviprashantkumar@gmail.com>
2023-09-29 19:32:56 +0530

    fix: access to bookmarks after signin

--- client/middleware.ts
+++ client/middleware.ts
@@ -1,3 +1,13 @@
-export { default } from "next-auth/middleware";
+import { NextRequest, NextResponse } from 'next/server';
 
-export const config = { matcher: ["/bookmarks", "/write"] };
+export default async function middleware(req: NextRequest) {
+  const path = req.nextUrl.pathname;
+
+  if (!req.cookies.get("next-auth.session-token")) {
+    return NextResponse.redirect(new URL(`/api/auth/signin?callbackUrl=${path}`, req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = { matcher: ["/bookmarks", "/write"] }
