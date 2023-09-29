1cda3d85392ecf5720290929dc426be8856ad50c
SaanviKumar13 <saanviprashantkumar@gmail.com>
2023-09-29 23:13:44 +0530

    feat: add responsivity

--- client/app/write/page.tsx
+++ client/app/write/page.tsx
@@ -13,7 +13,7 @@ const CreatePage = () => {
       <div className="flex h-screen w-full flex-col">
         <Navbar />
         <div className="p-2 justify-center h-full mt-8 px-5 md:px-24 lg:mt-16 lg:px-32 ">
-          <div className="flex justify-between gap-5 items-center">
+          <div className="flex flex-col lg:flex-row items-end lg:justify-between gap-5 lg:items-center">
             <div className="flex flex-row">
               <button className="flex gap-2 whitespace-nowrap text-xs h-6 px-3 py-1 rounded-3xl md:text-base md:h-10 md:px-5 md:py-2  text-gray-400 hover:text-kz-primary hover:border-kz-primary hover:bg-kz-button transition-all">
                 <Image className="h-4 md:h-6" />
@@ -32,28 +32,30 @@ const CreatePage = () => {
             </button>
           </div>
           <form className="px-5">
-            <textarea
-              id="title"
-              className="mt-5 w-full text-white bg-transparent text-2xl resize-none outline-none lg:text-5xl"
-              placeholder="Title"
-            ></textarea>
-            <textarea
-              id="tagline"
-              className={
-                (subtitle
-                  ? "resize-none outline-none w-full text-kz-secondary ml-2 bg-transparent text-xs md:text-base lg:text-xl"
-                  : "hidden") + ""
-              }
-              placeholder="Tagline"
-            ></textarea>
-            <div className="flex justify-evenly gap-20 text-white">
+            <div className="flex flex-col">
               <textarea
-                className="outline-none border-none w-[40vw] h-[60vh] overflow-x-hidden text-base resize-none p-5 bg-kz-card-light rounded-2xl"
+                id="title"
+                className="mt-5 w-full text-white bg-transparent text-2xl resize-none outline-none lg:text-5xl"
+                placeholder="Title"
+              ></textarea>
+              <textarea
+                id="tagline"
+                className={
+                  (subtitle
+                    ? "resize-none outline-none w-full text-kz-secondary ml-2 bg-transparent text-xs md:text-base lg:text-xl"
+                    : "hidden") + ""
+                }
+                placeholder="Tagline"
+              ></textarea>
+            </div>
+            <div className="flex flex-col lg:flex-row justify-evenly gap-20 text-white">
+              <textarea
+                className="outline-none border-none w-[75vw] lg:w-[40vw] h-[90vh] overflow-x-hidden text-base resize-none p-5 bg-kz-card-light rounded-2xl"
                 value={markdownInput}
                 placeholder="Type Away..."
                 onChange={(e) => setMarkdownInput(e.target.value)}
               ></textarea>
-              <div className="p-5 outline-none border-none w-[40vw] overflow-x-hidden text-base resize-none bg-kz-card-light rounded-xl">
+              <div className="p-5 mb-10 outline-none border-none w-[75vw] h-fit lg:h-[90vh] lg:w-[40vw] overflow-x-hidden text-base resize-none bg-kz-card-light rounded-xl">
                 <ReactMarkdown
                   children={markdownInput}
                   remarkPlugins={[remarkGfm]}
