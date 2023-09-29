db5dcce68a3ab137b27a7bc45fb8b07317996dc4
SaanviKumar13 <saanviprashantkumar@gmail.com>
2023-09-29 11:09:52 +0530

    feat: modify db field

--- client/app/api/auth/[...nextauth]/options.ts
+++ client/app/api/auth/[...nextauth]/options.ts
@@ -1,11 +1,21 @@
 import { dbClientPromise } from "@/app/utils/mongo";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import type { NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
 
 export const options: NextAuthOptions = {
   providers: [
     GoogleProvider({
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          bookmarks:[],
+          is_admin:false,
+        };
+      },
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
       authorization: {
@@ -24,3 +34,5 @@ export const options: NextAuthOptions = {
     databaseName: process.env.DB_NAME,
   }),
 };
+
+
