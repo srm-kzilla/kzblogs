d8404f35c51d3827755d29095b7f5a602267cba2
SaanviKumar13 <saanviprashantkumar@gmail.com>
2023-09-30 10:45:19 +0530

    feat: add button component

--- client/app/write/page.tsx
+++ client/app/write/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Button from "@/components/Button";
 import Navbar from "@/components/Navbar";
 import { Image, PenSquareIcon } from "lucide-react";
 import { useState } from "react";
@@ -15,21 +16,16 @@ const CreatePage = () => {
         <div className="p-2 justify-center h-full mt-8 px-5 md:px-24 lg:mt-16 lg:px-32 ">
           <div className="flex flex-col lg:flex-row items-end lg:justify-between gap-5 lg:items-center">
             <div className="flex flex-row">
-              <button className="flex gap-2 whitespace-nowrap text-xs h-6 px-3 py-1 rounded-3xl md:text-base md:h-10 md:px-5 md:py-2  text-gray-400 hover:text-kz-primary hover:border-kz-primary hover:bg-kz-button transition-all">
+              <Button variant="primary">
                 <Image className="h-4 md:h-6" />
                 <p>Add Cover Image</p>
-              </button>
-              <button
-                className="flex gap-2 whitespace-nowrap text-xs h-6 px-3 py-1 rounded-3xl md:text-base md:h-10 md:px-5 md:py-2  text-gray-400 hover:text-kz-primary hover:border-kz-primary hover:bg-kz-button transition-all"
-                onClick={() => setSubtitle(!subtitle)}
-              >
+              </Button>
+              <Button variant="primary" onClick={() => setSubtitle(!subtitle)}>
                 <PenSquareIcon className="h-4 md:h-6" />
                 <p>Add Subtitle</p>
-              </button>
+              </Button>
             </div>
-            <button className="h-6 py-1 px-2 text-xs rounded-3xl md:text-base md:h-10 md:px-5 md:py-2 border border-kz-highlight-light border-solid text-kz-button hover:text-kz-primary hover:border-kz-primary hover:bg-kz-button transition-all duration-500 ">
-              Publish
-            </button>
+            <Button variant="secondary">Publish</Button>
           </div>
           <form className="px-5">
             <div className="flex flex-col">
