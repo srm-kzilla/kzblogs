015dbc30e99436caa6b871f6d0c847bf2bce30b2
Rushi Patel <rushinasa06@gmail.com>
2023-10-02 00:03:32 +0530

    fix: middleware

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["all"],
+    allow_origins=["*"],
     allow_credentials=True,
-    allow_methods=["all"],
-    allow_headers=["all"]
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 app.include_router(admin.router, prefix="/admin", tags=["admin"])
 app.include_router(routes.router, prefix="/api", tags=["api"])
@@ -20,4 +21,3 @@ verify_auth(app)
 @app.get("/")
 async def root(request: Request):
     return Response("KZBLOGS API")
-
