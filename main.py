5b550ed67fb07d56a88a3f42d5b035ccbbb53a60
Manaya20 <pachpor.manaya@gmail.com>
2023-10-01 22:08:04 +0530

    feat: add CORS middleware

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, Response, Request
 from routes import admin, routes
 from middleware import verify_auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 app.include_router(admin.router, prefix="/admin", tags=["admin"])
@@ -11,3 +12,11 @@ verify_auth(app)
 @app.get("/")
 async def root(request: Request):
     return Response("KZBLOGS API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["all"],
+    allow_headers=["all"]
+)
