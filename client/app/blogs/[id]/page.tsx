b7467e4e081d996f37cd862d409bf776d7649cf6
ShivamBansal07 <sb2368@srmist.edu.in>
2023-11-30 19:42:37 +0530

    chore: refactor code

--- client/app/blogs/[id]/page.tsx
+++ client/app/blogs/[id]/page.tsx
@@ -2,35 +2,30 @@ import Navbar from "@/components/Navbar";
 import { getBlog, toggleLike } from "@/utils/api";
 import { Heart, UserIcon } from "lucide-react";
 import Markdown from "react-markdown";
+import { useSession } from "next-auth/react";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const blog : Blog = await getBlog(params.id);
+  const blog: Blog = await getBlog(params.id);
 
   return (
     <div className="w-full">
       <Navbar />
       <div className="flex-col justify-center py-10">
         <div className="flex justify-center">
-          <div className="prose prose-invert px-4 overflow-auto max-w-[86ch] xl:mx-auto">
-            <div className="text-center">
-              <h1 className="text-4xl sm:text-5xl mb-5">{blog.name}</h1>
-              <div className="flex justify-center text-lg m-1">
-                <div className="flex flex-col sm:flex-row">
-                  <div className="flex px-2 sm:mb-2 justify-center items-center text-2xl sm:text-lg">
-                    <UserIcon className="pr-1 w-fit" />
-                    {blog.author}
-                  </div>
-                  <div className="flex justify-center items-center text-base sm:text-lg">
-                    <div className="flex px-2">
-                      <Heart className="pr-1 w-fit" />
-                      {Object.keys(blog.likes).length}
-                    </div>
-                  </div>
-                </div>
+          <div className="prose prose-invert px-4 overflow-auto max-w-[86ch] xl:mx-auto text-center">
+            <h1 className="text-3xl sm:text-5xl mb-5">{blog.name}</h1>
+            <div className="flex text-lg m-1 flex-col  sm:flex-row justify-center items-center">
+              <div className="flex px-2 mb-2 sm:mb-0 justify-center items-center text-sm sm:text-base">
+                <UserIcon className="pr-1 w-5 sm:w-6" />
+                {blog.author}
               </div>
-              <div className="text-left">
-                <Markdown>{blog.content}</Markdown>
+              <div className="flex px-2 justify-center items-center text-sm sm:text-base">
+                <Heart className="pr-1 w-5 sm:w-6" />
+                {Object.keys(blog.likes).length}
               </div>
+            </div>
+            <div className="flex justify-center">
+              <Markdown>{blog.content}</Markdown>
             </div>
           </div>
         </div>
