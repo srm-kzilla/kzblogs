c4485259e31d354f9d6ea4c80fe6d032d3a1067b
ShivamBansal07 <sb2368@srmist.edu.in>
2023-12-12 21:51:11 +0530

    chore: refactor blog page code and made buttons reactive

--- client/components/LikeButton.tsx
+++ client/components/LikeButton.tsx
@@ -1,17 +1,26 @@
 "use client";
-import { toggleLike, } from "@/utils/api";
+import { useState } from "react";
+import { toggleLike } from "@/utils/api";
 import { Heart } from "lucide-react";
 
-const LikeButton = ({
-  _id,
-  likes,
-}: Blog) => {
-  const isLiked = false;
+const LikeButton = ({ _id, likes, userId }: Blog & { userId: string }) => {
+  const [isLiked, setIsLiked] = useState(likes.includes(userId));
+  const [totalLikes, setTotalLikes] = useState(Object.keys(likes).length);
   return (
-    <button onClick={() => toggleLike(_id)}>
+    <button
+      onClick={() =>
+        userId
+          ? (toggleLike(_id),
+            setIsLiked((prevState) => !prevState),
+            setTotalLikes((prevLikes) =>
+              isLiked ? prevLikes - 1 : prevLikes + 1,
+            ))
+          : undefined
+      }
+    >
       <div className="flex px-2 justify-center items-center text-sm sm:text-base">
-        <Heart className="pr-1 w-5 sm:w-6" />
-        {Object.keys(likes).length}
+        <Heart className={`pr-1 w-5 sm:w-6 ${isLiked ? "text-red-500" : ""}`} />
+        {totalLikes}
       </div>
     </button>
   );
