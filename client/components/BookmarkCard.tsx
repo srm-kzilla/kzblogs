c542fe15071f58f5e38d66fdbef52d9b92445dac
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-01-08 12:33:25 +0530

    fix: bookmark overflow

--- client/components/BookmarkCard.tsx
+++ client/components/BookmarkCard.tsx
@@ -14,7 +14,7 @@ const BookmarkCard = async () => {
       {bookmarkNumber === 0 ? (
         <div className="bg-gradient-to-tr from-kz-highlight-dark via-kz-highlight-light via-70% to-kz-secondary to-100% p-5 w-full h-fit text-kz-secondary rounded-3xl">
           <div className="flex-col">
-            <h1 className="text-lg md:text-2xl ">Bookmarks</h1>
+            <h1 className="text-lg md:text-2xl">Bookmarks</h1>
             <p className="my-3 font-sans text-base">
               Nothing here yet :(
               <br />
@@ -29,7 +29,7 @@ const BookmarkCard = async () => {
             Bookmarked ( {bookmarkNumber} )
           </h1>
           <div className="flex max-h-40 no-scrollbar w-full flex-col mt-3">
-            {bookmarkBlogs.map(({ _id, name, content }: Blog) => (
+            {bookmarkBlogs.slice(0, 2).map(({ _id, name, content }: Blog) => (
               <div key={_id} className="flex items-center justify-left my-3">
                 <div className="mx-1 text-ellipsis w-full">
                   <p className="text-sm">{name}</p>
