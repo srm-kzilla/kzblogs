763977a4fcac81a68aed1cb95265d65df7da9e2c
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-01-09 21:51:19 +0530

    feat: add 404 page

--- client/app/blogs/[id]/page.tsx
+++ client/app/blogs/[id]/page.tsx
@@ -4,42 +4,51 @@ import LikeButton from "@/components/LikeButton";
 import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import { notFound } from "next/navigation";
 
 export default async function Page({ params }: { params: { id: string } }) {
-  const blog: Blog = await getBlog(params.id);
-  const { _id } = await getCurrentUser();
-  return (
-    <div className="w-full">
-      <Navbar />
-      <div className="flex-col justify-center py-10">
-        <div className="flex justify-center">
-          <div className="prose prose-invert px-4 overflow-auto max-w-[86ch] xl:mx-auto">
-            <h1 className="text-3xl text-center sm:text-5xl mb-3">
-              {blog.name}
-            </h1>
-            <div className="flex text-lg m-1 flex-col  sm:flex-row justify-center items-center">
-              <div className="flex px-2 mb-2 sm:mb-0 justify-center items-center text-sm sm:text-base">
-                <Image
-                  src={blog.author.image}
-                  alt=""
-                  className="rounded-full mr-1 w-5 h-5 sm:h-7 sm:w-7"
-                  width={40}
-                  height={40}
-                />
-                {blog.author.name}
+  try {
+    const blog: Blog = await getBlog(params.id);
+    if (!blog) {
+      throw new Error("Blog not found");
+    }
+
+    const { _id } = await getCurrentUser();
+    return (
+      <div className="w-full">
+        <Navbar />
+        <div className="flex-col justify-center py-10">
+          <div className="flex justify-center">
+            <div className="prose prose-invert px-4 overflow-auto max-w-[86ch] xl:mx-auto">
+              <h1 className="text-3xl text-center sm:text-5xl mb-3">
+                {blog.name}
+              </h1>
+              <div className="flex text-lg m-1 flex-col  sm:flex-row justify-center items-center">
+                <div className="flex px-2 mb-2 sm:mb-0 justify-center items-center text-sm sm:text-base">
+                  <Image
+                    src={blog.author.image}
+                    alt=""
+                    className="rounded-full mr-1 w-5 h-5 sm:h-7 sm:w-7"
+                    width={40}
+                    height={40}
+                  />
+                  {blog.author.name}
+                </div>
+                <LikeButton {...blog} userId={_id} />
               </div>
-              <LikeButton {...blog} userId={_id} />
-            </div>
-            <div className="">
-              <ReactMarkdown
-                children={blog.content}
-                remarkPlugins={[remarkGfm]}
-                className="prose prose-invert"
-              />
+              <div className="">
+                <ReactMarkdown
+                  children={blog.content}
+                  remarkPlugins={[remarkGfm]}
+                  className="prose prose-invert"
+                />
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } catch (e) {
+    notFound();
+  }
 }
---
+++ client/app/not-found.tsx
@@ -0,0 +1,27 @@
+import Button from "@/components/Button";
+import Navbar from "@/components/Navbar";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <Navbar />
+      <div className="flex flex-col items-center justify-center h-[86vh]">
+        <h2 className="text-7xl md:text-9xl text-kz-highlight-light font-bold mb-4">
+          404
+        </h2>
+        <p className="text-lg text-kz-secondary mb-3">
+          Looks like you are lost
+        </p>
+        <p className="text-xs text-kz-secondary mb-8">
+          The page you are looking for is not available
+        </p>
+        <Button variant="secondary">
+          <Link legacyBehavior href="/">
+            <a className="text-white">Go back Home</a>
+          </Link>
+        </Button>
+      </div>
+    </div>
+  );
+}
