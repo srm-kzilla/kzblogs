87281d980026fb9f1e1c69b62dd97e4f939b860d
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-07-03 18:46:46 +0530

    feat: add loader

---
+++ client/app/loading.tsx
@@ -0,0 +1,11 @@
+import Loader from "@/components/Loader";
+
+const Loading = () => {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <Loader />
+    </div>
+  );
+};
+
+export default Loading;
